39d154830ba90fc3af0fca6ef85951dfd4c5fef8
Ganesh P <128501278+GaneshInnovator@users.noreply.github.com>
2024-12-01 16:54:55 +0530

    Update strings.dart

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -6,7 +6,7 @@ class AppStrings {
   static const String uploadButton = "Upload";
   static const String confirmButton = "Confirm";
   static const String cancelButton = "Cancel";
-  static const String imagePreviewTitle = "Image Preview"; // New title for ImagePreviewPage
+  static const String imagePreviewTitle = "Image Preview"; 
 
   static const String errorMessage = "Error occurred. Please try again.";
   static const String permissionDenied = "Permission denied. Please allow access.";
